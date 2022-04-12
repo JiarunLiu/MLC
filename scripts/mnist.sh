00ecce589e7033201750929aa65f21a6d6ad0568
JiarunLiu <jiarunliu@foxmail.com>
2022-04-12 15:48:11 +0800

    [UPD] remove discard options

--- scripts/mnist.sh
+++ scripts/mnist.sh
@@ -2,7 +2,7 @@ for seed in 1 2 3 4 5
 do
   python MLC.py --dir experiment/mnist/mlc/sn_0.2/trial_${seed} \
    --dataset 'mnist' --noise_type sn --noise 0.2 \
-   --optim "Adam" --discard  0 --forget-rate 0.2 --lambda1 1000 \
+   --optim "Adam" --forget-rate 0.2 --lambda1 1000 \
    --stage1 30 --stage2 140 --epochs 320 --backbone mlp --random-seed ${seed} &
 
   python MLC.py --dir experiment/mnist/mlc/sn_0.4/trial_${seed} \
